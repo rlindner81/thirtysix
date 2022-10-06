a253d13769d3faeea32d71649a381b2e3ad9d292
Richard Lindner <richard.lindner@sap.com>
2022-10-06 14:53:44 +0200

    very simple but it works

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,116 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="app">
+    <div class="header">
+      <button @click="prevSet">&langle;&langle;</button>
+      <button @click="questionIndex > 0 && questionIndex--">&langle;</button>
+      <button
+        @click="questionIndex < sets[setIndex].length - 1 && questionIndex++"
+      >
+        &rangle;
+      </button>
+      <button @click="nextSet">&rangle;&rangle;</button>
+    </div>
+    <div class="content">
+      <h3>Set {{ setNumberNames[setIndex] }}</h3>
+      <p v-if="sets[setIndex][questionIndex]">
+        {{ questionIndex + 1 }}. {{ sets[setIndex][questionIndex] }}
+      </p>
+    </div>
+    <div class="footer"></div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex !== 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}
+
+const setNumberNames = ["One", "Two", "Three"];
+
+const sets = [
+  shuffle([
+    "Given the choice of anyone in the world, whom would you want as a dinner guest?",
+    "Would you like to be famous? In what way?",
+    "Before making a telephone call, do you ever rehearse what you are going to say? Why?",
+    "What would constitute a “perfect” day for you?",
+    "When did you last sing to yourself? To someone else?",
+    "If you were able to live to the age of 90 and retain either the mind or body of a 30-year-old for the last 60 years of your life, which would you want?",
+    "Do you have a secret hunch about how you will die?",
+    "Name three things you and your partner appear to have in common.",
+    "For what in your life do you feel most grateful?",
+    " If you could change anything about the way you were raised, what would it be?",
+    " Take four minutes and tell your partner your life story in as much detail as possible.",
+    " If you could wake up tomorrow having gained any one quality or ability, what would it be?",
+  ]),
+  shuffle([
+    "If a crystal ball could tell you the truth about yourself, your life, the future or anything else, what would you want to know?",
+    "Is there something that you’ve dreamed of doing for a long time? Why haven’t you done it?",
+    "What is the greatest accomplishment of your life?",
+    "What do you value most in a friendship?",
+    "What is your most treasured memory?",
+    "What is your most terrible memory?",
+    "If you knew that in one year you would die suddenly, would you change anything about the way you are now living? Why?",
+    "What does friendship mean to you?",
+    "What roles do love and affection play in your life?",
+    "Alternate sharing something you consider a positive characteristic of your partner. Share a total of five items.",
+    "How close and warm is your family? Do you feel your childhood was happier than most other people’s?",
+    "How do you feel about your relationship with your mother?",
+  ]),
+  shuffle([
+    "Make three true “we” statements each. For instance, “We are both in this room feeling ... “",
+    "Complete this sentence: “I wish I had someone with whom I could share ... “",
+    "If you were going to become a close friend with your partner, please share what would be important for him or her to know.",
+    "Tell your partner what you like about them; be very honest this time, saying things that you might not say to someone you’ve just met.",
+    "Share with your partner an embarrassing moment in your life.",
+    "When did you last cry in front of another person? By yourself?",
+    "Tell your partner something that you like about them already.",
+    "What, if anything, is too serious to be joked about?",
+    "If you were to die this evening with no opportunity to communicate with anyone, what would you most regret not having told someone? Why haven’t you told them yet?",
+    "Your house, containing everything you own, catches fire. After saving your loved ones and pets, you have time to safely make a final dash to save any one item. What would it be? Why?",
+    "Of all the people in your family, whose death would you find most disturbing? Why?",
+    "Share a personal problem and ask your partner’s advice on how he or she might handle it. Also, ask your partner to reflect back to you how you seem to be feeling about the problem you have chosen.",
+  ]),
+];
 
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  methods: {
+    prevSet() {
+      if (this.setIndex > 0) {
+        this.setIndex--;
+        this.questionIndex = 0;
+      }
+    },
+    nextSet() {
+      if (this.setIndex < this.sets.length - 1) {
+        this.setIndex++;
+        this.questionIndex = 0;
+      }
+    },
+  },
+  data() {
+    return {
+      setIndex: 0,
+      questionIndex: 0,
+      sets,
+      setNumberNames,
+    };
   },
 };
 </script>
@@ -19,8 +120,23 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-size: larger;
+}
+
+button {
+  font-size: larger;
+  padding: 0.5rem 1rem;
+}
+
+.header {
+  display: flex;
+  gap: 1rem;
+}
+
+.content > * {
+  margin: 1rem;
+}
+.header {
+  margin: 2rem 1rem;
 }
 </style>
